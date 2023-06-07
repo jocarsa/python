aa4f37145b5a8706a1021bfeebbc352273e1ef97
jocarsa <info@josevicentecarratala.com>
2023-06-07 18:18:31 +0200

    Actualizar 002-error.acomment.py

---
+++ 001-Fundamentos de Python/001-Comentarios/002-error.acomment.py
@@ -0,0 +1,6 @@
+ debemos tener cuidado, porque, cuando creamos un comentario de una única línea, al pulsar el retorno de carro, todo el texto que escribamos en la siguiente línea, será considerado como código de Python.
+
+A continuación se muestra un ejemplo, dentro del cual, iniciamos un comentario de una única línea, pero ese comentario no abarca la segunda línea.
+
+Este programa proporciona un error, porque cuando intentamos ejecutarlo, el intérprete no es capaz de entender el significado de la segunda línea, puesto que no está escrito en una sintaxis que sea válida por Python, y por lo tanto, proporciona un error.
+
