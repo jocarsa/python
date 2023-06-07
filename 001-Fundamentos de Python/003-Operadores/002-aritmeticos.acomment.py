4ffc4621c1190e870b9866826164d5ebb30f806c
jocarsa <info@josevicentecarratala.com>
2023-06-07 18:56:53 +0200

    Actualizar 002-aritmeticos.acomment.py

---
+++ 001-Fundamentos de Python/003-Operadores/002-aritmeticos.acomment.py
@@ -0,0 +1,21 @@
+ A continuación, vamos a mostrar un ejemplo acerca del uso de los operadores aritméticos, que son los operadores más sencillos de utilizar en Vitoria en cualquier lenguaje de programación.
+
+El objetivo de los operadores aritméticos es poder realizar operaciones matemáticas sencillas, Dalex, por ejemplo, la suma, la resta, la multiplicación, o la división.
+
+Por ello, en el ejemplo que se muestra a continuación, aparecen ejemplos de estas operaciones básicas, que son las siguientes.
+
+Para la operación aritmética de suma, utilizaremos el signo más.
+
+Para la operación aritmética de resta, utilizaremos le-medio.
+
+Para la operación médica de multiplicación, usaremos el*.
+
+Para la operación aritmética de división, utilizaremos la barra, carácter que no hay que confundir con la\.
+
+Y por último, para realizar una operación de obtención del resto entero de la división, utilizaremos el símbolo del porcentaje.
+
+En términos generales, el lenguaje de programación Python intenta mantener el tipo de datos con los cuales estamos trabajando.
+
+Por lo tanto, tanto en la suma, como en la resta, como en la multiplicación, podemos observar que, si los operandos de la operación son números enteros, el resultado intenta también ser un número entero.
+
+Sin embargo, en el caso de la división, aunque los operandos de base son números enteros, el resultado de la operación aritmética tiene decimales, por lo cual, el sistema no tiene más remedio que acabar utilizando un tipo de coma flotante.
