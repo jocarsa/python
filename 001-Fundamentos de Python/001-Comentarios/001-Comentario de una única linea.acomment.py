15cf1fc4dde59c4b0292b910d0e420833ccb4875
jocarsa <info@josevicentecarratala.com>
2023-06-07 18:16:53 +0200

    Actualizar 001-Comentario de una única linea.acomment.py

---
+++ 001-Fundamentos de Python/001-Comentarios/001-Comentario de una única linea.acomment.py
@@ -0,0 +1,16 @@
+ En primer lugar, podemos introducir comentarios dentro de nuestro código fuente.
+
+Existen los comentarios de una única línea, que son los que se presentan en este ejercicio, y también disponemos de los comentarios de múltiples líneas.
+
+Con los comentarios de una única línea, arrancaremos un comentario con el #, y todos aquellos caracteres que estén a la derecha de ese símbolo, serán considerados como un comentario, y por lo tanto, serán ignorados por la intérprete.
+
+Los comentarios tienen tres funciones principales:
+
+En primer lugar, nos permiten dejarnos instrucciones para nosotros mismos, o bien para un futuro, o bien para cuando la complejidad del programa aumente demasiado, y necesitemos dejarnos pistas acerca del funcionamiento de nuestro propio código.
+
+En segundo lugar, cuando trabajamos en grupo, o en equipo, en muchas ocasiones necesitamos dejar códigos de referencia, comentarios, para aquellas personas que forman parte de nuestro equipo, como una forma de documentar nuestro código.
+
+En tercer lugar, en ocasiones, nos conviene silenciar parte del código con el que estamos trabajando, para lo cual, podemos utilizar comentarios.
+
+A continuación, por lo tanto, se muestra un ejemplo de los comentarios de una única línea, aplicados, en este caso, el lenguaje de programación Python.
+
