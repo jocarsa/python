9b0b90b7c9592216598564b56d471f018a6b5cd8
jocarsa <info@josevicentecarratala.com>
2023-06-07 19:32:47 +0200

    Actualizar 003-constantes.acomment.py

---
+++ 001-Fundamentos de Python/004-Estructuras de datos/001-Variables/003-constantes.acomment.py
@@ -0,0 +1,19 @@
+ de la misma forma que las variables son contenedores de información, cuyo propósito consiste en contener información que puede variar a lo largo de la ejecución de un programa, también existe, en muchos lenguajes de programación, el concepto de constante, representando a un contenedor de información al cual se le asigna un valor al principio de la ejecución del programa, y ese valor se mantendrá uniforme. Es variable hasta el final de la ejecución.
+
+En Python, como tal, no existe, de forma dura, el concepto de constante, pero al igual que ocurre en muchos otros lenguajes de programación, se anima a los desarrolladores a introducir las mayúsculas dentro de los identificadores de las constantes, para que, en base a una serie de reglas de buenas prácticas, otros programadores, o incluso uno mismo, puedan saber que ese valor debe ser tratado como una constante.
+
+Sin embargo, tal y como podemos comprobar, en el ejercicio que se presenta a continuación, podemos cambiar el valor de una constante, y realmente, comprobaremos cómo se comportará como una variable.
+
+Llegados a este punto, es conveniente indicar que existen una serie de reglas de buenas prácticas, ampliamente adoptadas por la gran mayoría de programadores.
+
+Estas reglas de buenas prácticas tienen como objetivo escribir código de una forma que sea lo más estándar posible, para que el código sea entendible.
+
+En cuanto al uso de las mayúsculas y minúsculas, la reglas de buenas prácticas suelen coincidir en que:
+
+Los nombres de las variables, o propiedades, en el caso de las clases, serán sustantivos, irán en minúsculas, y en el caso de contener varias palabras, se prefiere que esas varias palabras vayan separadas por guiones bajos.
+
+Los nombres de las funciones, empezarán con minúsculas, y generalmente deberán tener un verbo, en primer lugar, y un complemento directo, o algún otro tipo de complemento, en segundo lugar. Empezando con minúsculas, se aplicará la nomenclatura Camel case, mediante la cual, la primera palabra, el verbo, irá con mayúsculas, y a partir de la segunda palabra, irán las palabras pegadas, las unas a las otras, y la primera letra a partir de la segunda palabra será con mayúsculas.
+
+En cuanto a los nombres de las constantes, tal y como se trabaja en esta unidad didáctica, eran todas en mayúsculas.
+
+Y en cuanto a los nombres de las clases, o bien los objetos instancia dos a partir de las clases, tendrán la primera letra en mayúsculas, y el resto de letras en minúscula. 
